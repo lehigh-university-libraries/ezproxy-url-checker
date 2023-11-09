e7ec2425a0b6daea19ceaeb32aeab2ce3ac15040
Joe Corall <jjc223@lehigh.edu>
2023-11-09 10:56:59 -0500

    Add lint-test workflow

---
+++ scripts/test.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+EXIT_CODE=0
+
+echo "Checking domains in config return 200s"
+URLS=(
+  "https://lts.lehigh.edu"
+  "https://preserve.lehigh.edu"
+  "https://$RANDOM.lehigh.edu"
+  "https://$RANDOM.$RANDOM.lehigh.edu"
+
+)
+for URL in "${URLS[@]}"; do
+  echo "Checking $URL"
+  STATUS_CODE=$(curl -w '%{http_code}' \
+    -o /dev/null \
+    -s \
+    "http://localhost:8888/proxyUrl?url=$URL")
+  echo -e "\t$STATUS_CODE"
+  if [ ! ${STATUS_CODE} -eq 200 ]; then
+    EXIT_CODE=1
+  fi
+done
+
+echo "Checking domains not in config throw 404s"
+URLS=(
+  "https://scholar.google.com"
+)
+for URL in "${URLS[@]}"; do
+  echo "Checking $URL"
+  STATUS_CODE=$(curl -w '%{http_code}' \
+    -o /dev/null \
+    -s \
+    "http://localhost:8888/proxyUrl?url=$URL")
+  echo -e "\t$STATUS_CODE"
+  if [ ${STATUS_CODE} -eq 200 ]; then
+    EXIT_CODE=1
+  fi
+done
+
+exit $EXIT_CODE
