28351762161bfa9254ce33c99efcf360b09e7404
Joe Corall <jjc223@lehigh.edu>
2023-11-09 14:19:23 -0500

    Add test coverage for IncludeFile (#6)

--- scripts/test.sh
+++ scripts/test.sh
@@ -13,15 +13,15 @@ URLS=(
   "https://preserve.lehigh.edu"
   "https://$RANDOM.lib.lehigh.edu"
   "https://$RANDOM.$RANDOM.lib.lehigh.edu"
-
+  "https://scholar.google.com"
 )
 for URL in "${URLS[@]}"; do
-  echo "Checking ${URL}"
+  echo -e "\tChecking ${URL}"
   STATUS_CODE=$(curl -w '%{http_code}' \
     -o /dev/null \
     -s \
     "http://localhost:8888/proxyUrl?url=$URL")
-  echo -e "\t${STATUS_CODE}"
+  echo -e "\t\t${STATUS_CODE}"
   if [ ! "${STATUS_CODE}" -eq 200 ]; then
     EXIT_CODE=1
   fi
@@ -29,17 +29,17 @@ done
 
 echo "Checking domains not in config throw 404s"
 URLS=(
-  "https://scholar.google.com"
+  "https://lts.lehigh.COM"
   "https://$RANDOM.lehigh.edu"
   "https://$RANDOM.$RANDOM.lehigh.edu"
 )
 for URL in "${URLS[@]}"; do
-  echo "Checking ${URL}"
+  echo -e "\tChecking ${URL}"
   STATUS_CODE=$(curl -w '%{http_code}' \
     -o /dev/null \
     -s \
     "http://localhost:8888/proxyUrl?url=$URL")
-  echo -e "\t${STATUS_CODE}"
+  echo -e "\t\t${STATUS_CODE}"
   if [ ! "${STATUS_CODE}" -eq 404 ]; then
     EXIT_CODE=1
   fi
